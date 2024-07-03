720d0c680a0ba77d007e1d9dd9ff7c1bb2ad086e
- <->
2024-07-03 00:45:10 +0000

    resolve-namespace: Note expected result

--- resolve-namespace/entry.mjs
+++ resolve-namespace/entry.mjs
@@ -2,3 +2,5 @@ import { substance } from './aggregate.mjs'
 console.log(substance)
 // SyntaxError: The requested module './aggregate.mjs'
 // contains conflicting star exports for name 'substance'
+
+// Expected: Module { value: 'Curiouser and curiouser.' }
